798c89fd839c901c06f6a0388ccb067aa016650b
DESKTOP-H6JFRBD\jwkim <jwkim8936@naver.com>
2021-12-19 17:27:22 +0900

    Initial commit

---
+++ src/main/java/com/example/demo_1219_01/Demo121901Application.java
@@ -0,0 +1,13 @@
+package com.example.demo_1219_01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo121901Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Demo121901Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/demo_1219_01/Demo121901ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo_1219_01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demo121901ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
